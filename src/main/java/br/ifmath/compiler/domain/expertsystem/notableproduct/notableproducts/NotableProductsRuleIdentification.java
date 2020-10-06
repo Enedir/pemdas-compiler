5d34b46d37e44d73c1d50e2d8db02c18dafe57f5
Osmar José Hofman da Silva <osmarjhdas@hotmail.com>
2020-10-05 22:49:44 -0300

    NotableProduct : Refatorando e documentando classes de identificação e aplicação de fórmula

--- src/main/java/br/ifmath/compiler/domain/expertsystem/notableproduct/notableproducts/NotableProductsRuleIdentification.java
+++ src/main/java/br/ifmath/compiler/domain/expertsystem/notableproduct/notableproducts/NotableProductsRuleIdentification.java
@@ -16,6 +16,7 @@ public class NotableProductsRuleIdentification implements IRule {
 
     @Override
     public boolean match(List<ThreeAddressCode> source) {
+        //Sempre será executado o passo de identificar o tipo de produto notável
         return true;
     }
 
@@ -34,16 +35,23 @@ public class NotableProductsRuleIdentification implements IRule {
         return steps;
     }
 
+    /**
+     * Identifica qual o tipo de produto notável da equação inicial.
+     * @return {@link String} indicando qual o nome do tipo de produto notável.
+     */
     private String identify() {
         ExpandedQuadruple root = this.sources.findQuadrupleByResult(this.sources.getLeft());
+
         if (StringUtil.match(root.getArgument1(), RegexPattern.TEMPORARY_VARIABLE.toString())) {
             if (root.isPotentiation()) {
 
                 ExpandedQuadruple innerQuadruple = this.sources.findQuadrupleByResult(root.getArgument1());
 
+                //validação para equação do tipo (a +- b) ^ 2
                 if (root.getArgument2().equals("2"))
                     return innerQuadruple.isPlus() ? "Quadrado da soma de dois termos." : "Quadrado da diferença de dois termos.";
 
+                //validação para equação do tipo (a +- b) ^ 3
                 if (root.getArgument2().equals("3"))
                     return innerQuadruple.isPlus() ? "Cubo da soma de dois termos." : "Cubo da diferença de dois termos.";
 
@@ -59,7 +67,7 @@ public class NotableProductsRuleIdentification implements IRule {
                 String rightArgument1 = getInnerArgument(rightInnerQuadruple.getArgument1(), true);
                 String rightArgument2 = getInnerArgument(rightInnerQuadruple.getArgument2(), false);
 
-
+                //validação para equação do tipo (a + b) * (a - b)
                 if (leftArgument1.equals(rightArgument1) && leftArgument2.equals(rightArgument2))
                     return "Produto da soma pela diferença de dois termos.";
             }
