cdeda0e2da0b5540c1c596ed6d097ff6d4414671
Osmar José Hofman da Silva <osmarjhdas@hotmail.com>
2020-11-02 14:28:06 -0300

    Fatoration : Refatorando e documentando classes relacionadas a regra de trinomio do segundo grau

--- src/main/java/br/ifmath/compiler/domain/expertsystem/notableproduct/fatoration/seconddegreetrinomial/FatorationRuleSecondDegreeTrinomialConvertToDivisionFormula.java
+++ src/main/java/br/ifmath/compiler/domain/expertsystem/notableproduct/fatoration/seconddegreetrinomial/FatorationRuleSecondDegreeTrinomialConvertToDivisionFormula.java
@@ -38,40 +38,71 @@ public class FatorationRuleSecondDegreeTrinomialConvertToDivisionFormula impleme
         return steps;
     }
 
+    /**
+     * Ajusta as quádruplas para ficarem na fórmula: x^2 + (b/a)x + (c/a).
+     */
     private void convertToDivisionFormula() {
 
         ExpandedQuadruple argumentsQuadruple = this.source.getRootQuadruple();
 
+        //obtém o monomio do valor a
         Monomial monomialA = this.getMonomialWithExponent(argumentsQuadruple, 2);
+
+        //obtém o valor a em si
         String a = this.getElement(monomialA.toString());
 
+        //obtém o monomio do valor b
         Monomial monomialB = this.getMonomialWithExponent(argumentsQuadruple, 1);
+
+        //obtém o valor b em si
         String b = this.getElement(monomialB.toString());
 
+        //obtém o monomio do valor c
         Monomial monomialC = this.getMonomialWithExponent(argumentsQuadruple, 0);
+
+        //obtém o valor c em si
         String c = this.getElement(monomialC.toString());
 
+        //cria a parte da formula que é x^2 +
         argumentsQuadruple.setArgument1(monomialA.getLiteral());
         argumentsQuadruple.setOperator("+");
 
+        //cria a parte da formula que é (b/a)x +
         argumentsQuadruple = this.source.findQuadrupleByResult(argumentsQuadruple.getArgument2());
         argumentsQuadruple.setArgument1("(" + b + "/" + a + ")" + monomialB.getLiteral());
         argumentsQuadruple.setOperator("+");
 
+        //cria a parte da formula que é (c/a) +
         argumentsQuadruple.setArgument2("(" + c + "/" + a + ")");
     }
 
 
+    /**
+     * Obtém um monômio de acordo com seu expoente, e assim identificando qual é o valor a, b e c.
+     *
+     * @param iterationQuadruple {@link ExpandedQuadruple} que representa a quádrupla inicial para ser buscado o monômio.
+     * @param exponent           inteiro que representa o expoente a ser buscado, no qual podem ser:
+     *                           <ul>
+     *                              <li>2: significa um monomio do tipo x^2, ou seja, o valor a.</li>
+     *                              <li>1: significa um monomio do tipo x, ou seja, o valor b.</li>
+     *                              <li>0: significa um monomio que não tem parte literal, ou seja, o valor c.</li>
+     *                           </ul>
+     * @return {@link Monomial} representando o argumento encontrado para o valor de a, b ou c.
+     */
     private Monomial getMonomialWithExponent(ExpandedQuadruple iterationQuadruple, int exponent) {
         if (StringUtil.match(iterationQuadruple.getArgument1(), RegexPattern.TEMPORARY_VARIABLE.toString())) {
             return getMonomialWithExponent(this.source.findQuadrupleByResult(iterationQuadruple.getArgument1()), exponent);
         }
 
         Monomial iterationMonomialArgument = new Monomial(iterationQuadruple.getArgument1());
+
+        //se não for o valor c
         if (exponent != 0) {
             if (iterationMonomialArgument.getCoefficient() != 0 && iterationMonomialArgument.getLiteralDegree() == exponent)
                 return iterationMonomialArgument;
         } else {
+
+            //valida se é somente um número diferente de 0
             if (StringUtil.match(iterationQuadruple.getArgument1(), RegexPattern.NATURAL_NUMBER.toString()) &&
                     !iterationQuadruple.getArgument1().equals("0")) {
                 return iterationMonomialArgument;
@@ -87,31 +118,53 @@ public class FatorationRuleSecondDegreeTrinomialConvertToDivisionFormula impleme
 
         iterationMonomialArgument = new Monomial(iterationQuadruple.getArgument2());
 
+        //se não for o valor c
         if (exponent != 0) {
             if (iterationMonomialArgument.getCoefficient() != 0 && iterationMonomialArgument.getLiteralDegree() == exponent)
                 return iterationMonomialArgument;
         } else {
+
+            //valida se é somente um número diferente de 0
             if (StringUtil.match(iterationQuadruple.getArgument2(), RegexPattern.NATURAL_NUMBER.toString()) &&
                     !iterationQuadruple.getArgument2().equals("0")) {
                 return iterationMonomialArgument;
             }
         }
+        //se não identificar nenhum monomio para valor a, b ou c
         return new Monomial();
     }
 
+    /**
+     * Obtém o valor (a,b,c) a partir de um {@code argument}.
+     *
+     * @param argument {@link String} que representa um monômio que contém o valor desejado.
+     * @return {@link String} que representa somente o valor a, b ou c.
+     */
     private String getElement(String argument) {
         Monomial monomial = new Monomial(argument);
         ExpandedQuadruple argumentQuadruple = this.source.findQuadrupleByArgument(argument);
+
+        //encontra o argumento nas quádruplas
+
+        //se o argumento for igual ao argument1
         if (argumentQuadruple.getArgument1().equals(argument)) {
+
+            //se for uma quádrupla "MINUS"
             if (argumentQuadruple.isNegative()) {
                 return "-" + monomial.getCoefficient();
             }
+
+            //se for a quádrupla inicial, quer dizer que é positivo
             if (this.source.getLeft().equals(argumentQuadruple.getResult())) {
                 return String.valueOf(monomial.getCoefficient());
             }
+
+            //verifica se é um valor positivo ou negativo e retorna
             ExpandedQuadruple father = this.source.findDirectFather(argumentQuadruple.getResult());
             return (father.isMinus()) ? "-" + monomial.getCoefficient() : String.valueOf(monomial.getCoefficient());
         }
+
+        //verifica se o argument2 é um valor positivo ou negativo e retorna
         return (argumentQuadruple.isMinus()) ? "-" + monomial.getCoefficient() : String.valueOf(monomial.getCoefficient());
     }
 }
--- src/main/java/br/ifmath/compiler/domain/expertsystem/notableproduct/fatoration/seconddegreetrinomial/FatorationRuleSecondDegreeTrinomialProduct.java
+++ src/main/java/br/ifmath/compiler/domain/expertsystem/notableproduct/fatoration/seconddegreetrinomial/FatorationRuleSecondDegreeTrinomialProduct.java
@@ -40,51 +40,100 @@ public class FatorationRuleSecondDegreeTrinomialProduct implements IRule {
         return steps;
     }
 
+    /**
+     * Ajusta as quádruplas para ficarem na fórmula de produto das raízes: a * (x - x') * (x - x'').
+     */
     private void transformToProductOfRoots() {
         ExpandedQuadruple root = this.source.getRootQuadruple();
         if (StringUtil.match(root.getArgument2(), RegexPattern.TEMPORARY_VARIABLE.toString())) {
 
+            //acha quádrupla que contém os valores de a, b e c
             ExpandedQuadruple innerQuadruple = this.source.findQuadrupleByResult(root.getArgument2());
+
+            //pega os valores respectivos
             int a = this.getVariable(innerQuadruple.getArgument1(), true);
             int b = this.getVariable(innerQuadruple.getArgument1(), false);
             int c = this.getVariable(innerQuadruple.getArgument2(), false);
 
+            //cria a parte da formula que é a *
             String variable = new Monomial(root.getArgument1()).getLiteralVariable();
             root.setArgument1(String.valueOf(a));
             root.setOperator("*");
 
+            /*é necessário sempre fazer bhaskara, pois soma e produto só é viável de forma manual e ambos
+             * dão o mesmo resultados*/
             Bhaskara bhaskara = new Bhaskara(a, b, c);
 
-            this.source.addQuadrupleToList(bhaskara.getX1Operator(), variable, String.valueOf(bhaskara.getFirstRoot()), innerQuadruple, true).setLevel(1);
-            this.source.addQuadrupleToList(bhaskara.getX2Operator(), variable, String.valueOf(bhaskara.getSecondRoot()), innerQuadruple, false).setLevel(1);
+            //cria a parte da formula que é a (x - x')
+            this.source.addQuadrupleToList(bhaskara.getX1Operator(), variable, String.valueOf(bhaskara.getFirstRoot()), innerQuadruple, true)
+                    .setLevel(1);
+
+            //cria a parte da formula que é a (x - x'')
+            this.source.addQuadrupleToList(bhaskara.getX2Operator(), variable, String.valueOf(bhaskara.getSecondRoot()), innerQuadruple, false)
+                    .setLevel(1);
+
+            //coloca a operação de * entre as duas partes acima
             innerQuadruple.setOperator("*");
         }
 
     }
 
+    /**
+     * Encontra os valores de a, b e c.
+     *
+     * @param argument {@link String} que contém o valor a ser encontrado.
+     * @param isA      booleano que vai indicar se é o valor a, para diferenciar quando o {@code argument} se refere
+     *                 ao argument1 da quadrupla.
+     * @return inteiro que representa o valor de a, b ou c.
+     */
     private int getVariable(String argument, boolean isA) {
+        //os valores serão encontrados através do parênteses, baseado na fórmula da regra anterior x^2 + (b/a)x + (c/a).
+
+        /*A partir da fórmula é possível ver que o valor de a sempre está diretamente antes de um ")", e os valores
+         * de b e c estão diretamente depois de um "(" */
         int parenthesesIndex = (isA) ? argument.indexOf(')') : argument.indexOf('(');
+
+
         int divideBarIndex = argument.indexOf('/');
+
+        /*A lógica abaixo é: se for valor a, é obtido o valor que está DEPOIS do "/" e ANTES do ")";
+         *  se for b ou c, é obtido o valor que está ANTES do "/" e DEPOIS do "(";
+         * */
         String variable = (isA) ? argument.substring(divideBarIndex + 1, parenthesesIndex) :
                 argument.substring(parenthesesIndex + 1, divideBarIndex);
+
         return Integer.parseInt(variable);
     }
 
+    /**
+     * Verificação que incida se os valores de x' e x'' são iguais. Isso simboliza que na verdade a regra que
+     * deve ser aplicada não é a de trinômio do segundo grau, mas sim a de trinômio quadrado perfeito.
+     *
+     * @param source {@link ThreeAddressCode} que contém todas as quádruplas.
+     * @return true caso as raízes (x' e x'') sejam iguais, simbolizando que deve ser aplicada a regra de trinômio
+     * quadrado perfeito. Caso contrário, será retornado false, indicando que o processamento está correto através da
+     * regra de trinômio do segundo grau.
+     */
     public boolean areRootsEqual(ThreeAddressCode source) {
         ExpandedQuadruple root = source.getRootQuadruple();
         if (StringUtil.match(root.getArgument2(), RegexPattern.TEMPORARY_VARIABLE.toString())) {
 
+            //obtém os valores de a, b e c
             ExpandedQuadruple innerQuadruple = source.findQuadrupleByResult(root.getArgument2());
             int a = this.getVariable(innerQuadruple.getArgument1(), true);
             int b = this.getVariable(innerQuadruple.getArgument1(), false);
             int c = this.getVariable(innerQuadruple.getArgument2(), false);
 
+            //verifica se as duas raizes são iguais
             Bhaskara bhaskara = new Bhaskara(a, b, c);
             return bhaskara.getFirstRoot().equals(bhaskara.getSecondRoot());
         }
         return false;
     }
 
+    /**
+     * Classe que representa a fórmula de bhaskara com seus valores de raízes (x' e x'') com seus respectivos operadores.
+     */
     private static class Bhaskara {
 
         private String x1, x2, x1Operator, x2Operator;
@@ -109,31 +158,53 @@ public class FatorationRuleSecondDegreeTrinomialProduct implements IRule {
             return x2Operator;
         }
 
+        /**
+         * Realiza o cálculo da fórmula de bhaskara em uma função do segundo grau
+         *
+         * @param a inteiro que representa o valor de a.
+         * @param b inteiro que representa o valor de b.
+         * @param c inteiro que representa o valor de c.
+         */
         private void calculate(int a, int b, int c) {
             int delta = (b * b) + (-4 * (a * c));
-            int divisor = 2 * a;
             double dividend = -b + Math.sqrt(delta);
+            int divisor = 2 * a;
 
+            //pegando a primeira raiz x'
             float x1 = (float) (dividend / divisor);
             this.x1Operator = (x1 < 0) ? "+" : "-";
 
+            //se for uma raiz exata retornará ela mesma, caso contrário será mantido na forma de fração
             this.x1 = (x1 % 1 == 0) ?
                     String.valueOf(Math.abs(Math.round(x1))) :
                     this.getReducedFraction(dividend, divisor);
 
             dividend = -b - Math.sqrt(delta);
 
+            //pegando a segunda raiz x''
             float x2 = (float) (dividend / divisor);
             this.x2Operator = (x2 < 0) ? "+" : "-";
+
+            //se for uma raiz exata retornará ela mesma, caso contrário será mantido na forma de fração
             this.x2 = (x2 % 1 == 0) ?
                     String.valueOf(Math.abs(Math.round(x2))) :
                     this.getReducedFraction(dividend, divisor);
 
         }
 
-        private String getReducedFraction(double a, double b) {
-            double gcd = MathOperatorUtil.gcd(a, b);
-            return Math.abs(Math.round(a / gcd)) + "/" + Math.abs(Math.round(b / gcd));
+        /**
+         * Obtém a forma de fração de dois valores.
+         *
+         * @param numerator   valor que representa o numerador.
+         * @param denominator valor que representa o denominador.
+         * @return {@link String} que contém a fração na seguinta forma: numerator/denominator
+         */
+        private String getReducedFraction(double numerator, double denominator) {
+            //obtém o maior divisor comum dos dois valores
+            double gcd = MathOperatorUtil.gcd(numerator, denominator);
+
+            //retorna a fração já reduzida ao máximo
+            return Math.abs(Math.round(numerator / gcd)) + "/" + Math.abs(Math.round(denominator / gcd));
         }
 
     }
